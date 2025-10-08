98659242919d5357b983d68bfdc1577b10f22b2f
princess <chinyere.orji@pau.edu.ng>
2025-10-08 23:05:20 +0100

    (practice1-4)

---
+++ week-2/practice_1.rs
@@ -0,0 +1,29 @@
+fn main(){
+	println!("The course learning outcome is as follows");
+	println!("1. Distinguish between compter and computer progreamming,/n
+			2. Develop some teechniquws in computer science,/n
+			3. understand the different areas of study in computer science,/n
+			4. Conversant with applications of computer science,/n
+			5. Navigate the career prospect in computer science,/n
+			6. conversant with computer programming components." );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
