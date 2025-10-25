95ddb7440e6f85c1de9636d24c9658dfb7a3bd23
princess <chinyere.orji@pau.edu.ng>
2025-10-25 21:19:31 +0100

    project 1 complete

---
+++ week_4/project1/src/main.rs
@@ -0,0 +1,35 @@
+//Rust Program to find the roots of a quadratic equation 
+//given the values a,b,c
+use std::io;
+fn main() {
+    println!("The Value a is:");
+    let mut a =String::new();
+    io::stdin().read_line(&mut a).expect("not a valid number");
+    let a:f32 = a.trim().parse().expect("not a  valid number");
+
+    println!("The Value b is");
+      let mut b=String::new();
+    io::stdin().read_line(&mut b).expect("not a valid number");
+    let b:f32 = b.trim().parse().expect("not a  valid number");
+
+    println!("The value of c is");
+   let mut c =String::new();
+    io::stdin().read_line(&mut c).expect("not a valid number");
+    let c:f32 = c.trim().parse().expect("not a  valid number");
+
+    let discriminant: f32  = b*b - 4.0 * a * c;
+    if discriminant >0.0{
+    let root1: f32 =( -b + discriminant.sqrt())/(2.0 *a);
+    let root2:f32 = (-b -discriminant.sqrt())/(2.0 * a);
+    println!("The roots are {} and {}",root1,root2);
+}
+else if discriminant <0.0{
+    let _root : f32 = -b/(2.0 * a);
+    println!("The one real root is:{}",_root);
+
+}
+else{
+    println!("No real root");           
+}
+
+}
---
+++ week_4/project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+C:\Users\princ\Documents\c.orjiCOS101\week_4\project1\target\debug\project1.exe: C:\Users\princ\Documents\c.orjiCOS101\week_4\project1\src\main.rs
