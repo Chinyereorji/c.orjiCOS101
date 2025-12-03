2f8cbaf94bfb6256302bd956ff46070c41e08dd9
princess <chinyere.orji@pau.edu.ng>
2025-12-03 17:48:57 +0100

    passed a structure to a function

---
+++ week10/practice8/target/debug/deps/practice8.d
@@ -0,0 +1,5 @@
+C:\Users\princ\Documents\c.orjiCOS101\week10\practice8\target\debug\deps\practice8.d: src\main.rs
+
+C:\Users\princ\Documents\c.orjiCOS101\week10\practice8\target\debug\deps\practice8.exe: src\main.rs
+
+src\main.rs:
